ba365590f786490135a54b83f20b175bb84c3e55
MikkelKPersson <43847844+MikkelKPersson@users.noreply.github.com>
2019-05-04 18:32:20 +0200

    Initial commit

---
+++ app/src/main/java/com/sevencrayons/compass/CompassActivity.java
@@ -0,0 +1,127 @@
+package com.sevencrayons.compass;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class CompassActivity extends AppCompatActivity {
+
+    private static final String TAG = "CompassActivity";
+
+    private Compass compass;
+    private ImageView arrowView;
+    private ImageView roseView;
+    private TextView sotwLabelDegrees;
+    private TextView sotwLabelDirection;// SOTW is for "side of the world"
+
+    private float currentAzimuth;
+    private SOTWFormatter sotwFormatter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        sotwFormatter = new SOTWFormatter(this);
+
+        arrowView = findViewById(R.id.main_image_hands);
+        sotwLabelDegrees = findViewById(R.id.sotw_label_degree);
+        sotwLabelDirection = findViewById(R.id.sotw_label_direction);
+        roseView = findViewById(R.id.main_image_dial);
+
+        setupCompass();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "start compass");
+        compass.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        compass.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        compass.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "stop compass");
+        compass.stop();
+    }
+
+    private void setupCompass() {
+        compass = new Compass(this);
+        Compass.CompassListener cl = getCompassListener();
+        compass.setListener(cl);
+    }
+
+    private void adjustRose(float azimuth) {
+//        Log.d(TAG, "will set rotation from " + currentAzimuth + " to "
+//                + azimuth);
+
+//        float difference = Math.abs(currentAzimuth - azimuth);
+//        float maxPivotValue = 0.5f;
+//        float pivotValue;
+//                pivotValue=  difference;
+//        if (difference > maxPivotValue) {
+//            pivotValue = maxPivotValue;
+//        }
+//        System.out.println(difference);
+
+        Animation rotateRose = new RotateAnimation(-currentAzimuth, -azimuth,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+
+        rotateRose.setDuration(500);
+        rotateRose.setRepeatCount(0);
+        rotateRose.setFillAfter(true);
+
+        //System.out.println(difference);
+//        if(difference > 0.05) {
+         roseView.startAnimation(rotateRose);
+//        }
+        currentAzimuth = azimuth;
+
+
+
+       // System.out.println(-azimuth);
+    }
+
+    private void adjustSotwLabel(float azimuth) {
+        sotwLabelDegrees.setText(sotwFormatter.formatDegree(azimuth));
+        sotwLabelDirection.setText(sotwFormatter.formatDirection(azimuth));
+    }
+
+    private Compass.CompassListener getCompassListener() {
+        return new Compass.CompassListener() {
+            @Override
+            public void onNewAzimuth(final float azimuth) {
+                // UI updates only in UI thread
+                // https://stackoverflow.com/q/11140285/444966
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adjustRose(azimuth);
+                        adjustSotwLabel(azimuth);
+                    }
+                });
+            }
+        };
+    }
+}
